8ef311cf086054740d3d63c4b56aabff6ebb0c22
João Varela <joaocvarela@gmail.com>
2024-08-31 18:56:39 +0100

    Finishing the previous commit.

--- blast_parser/BlastParser.swift
+++ blast_parser/BlastParser.swift
@@ -5,20 +5,47 @@
 //  Created by João Varela on 31/08/2024.
 //
 
+import Foundation
 import ArgumentParser
-
 
 @main
 struct BlastParser: ParsableCommand {
-    @Argument var inputFile: String
-    @Argument var outputFile: String
+    @Argument(help: "Path to rankedtaxonomy.dmp file to be imported.")
+    var inputFile: String
+    @Option(name: [.short, .customLong("output")],
+            help: "Path to the output CSV file, which will be overwritten if it already exists.")
+    var outputFile: String? = nil
+    
+    static let configuration = CommandConfiguration(
+            abstract: "Imports an NCBI ranked taxonomy dump file an exports it into a PostGresSQL database",
+            usage: """
+                blast_parser [OPTIONS] <input-file>
+                """)
     
     mutating func run() throws {
-        print("""
-            Counting words in '\(inputFile)' \
-            and writing the result into '\(outputFile)'.
-            """)
-            
-        // Read 'inputFile', count the words, and save to 'outputFile'.
+        let inputURL = URL(fileURLWithPath: inputFile)
+        guard FileManager.default.fileExists(atPath: inputFile) else {
+            throw RuntimeError("Input file at \(inputFile) not found.")
+        }
+        
+        var outputPath = String()
+        if outputFile == nil {
+            let oldFilenameURL = inputURL.deletingPathExtension()
+            outputPath = oldFilenameURL.appendingPathExtension("csv").path
+        } else {
+            outputPath = outputFile!
+        }
+        
+        let database = Database(path: inputFile, outputPath: outputPath)
+        database.parse()
     }
 }
+
+struct RuntimeError: Error, CustomStringConvertible {
+    var description: String
+    
+    init(_ description: String) {
+        self.description = description
+    }
+}
+
--- blast_parser/Package.swift
+++ blast_parser/Package.swift
@@ -5,4 +5,16 @@
 //  Created by João Varela on 31/08/2024.
 //
 
-import Foundation
+import PackageDescription
+
+let package = Package(
+    name: "BlastParser",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "blast_parser",
+            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]),
+    ]
+)
