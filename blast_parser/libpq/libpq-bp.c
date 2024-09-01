f777d0278e485b1fa7f370c5dc1c2bab3d2b4c38
João Varela <joaocvarela@gmail.com>
2024-09-01 17:55:12 +0100

    Added libpq

---
+++ blast_parser/libpq/libpq-bp.c
@@ -0,0 +1,14 @@
+//
+//  libpq.c
+//  blast_parser
+//
+//  Created by João Varela on 01/09/2024.
+//
+
+#include "libpq-bp.h"
+
+void createDatabase(const char *database,
+                    const char *table) {
+    printf("Create database %s", database);
+    printf(" with table %s", table);
+}
