11f5d52a2962d7f208df1524cabaa27ded1f3c5b
João Varela <joaocvarela@gmail.com>
2024-09-28 12:04:06 +0100

    Added @retroactive to the FileHandle extension to avoid future conflicts

--- blast_parser/Console.swift
+++ blast_parser/Console.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-extension FileHandle: TextOutputStream {
+extension FileHandle: @retroactive TextOutputStream {
   public func write(_ string: String) {
     let data = Data(string.utf8)
     self.write(data)
