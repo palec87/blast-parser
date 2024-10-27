fd3a6b8b761dde2ad5d2024d0ed3357f70f15de2
João Varela <joaocvarela@gmail.com>
2024-10-27 12:42:50 +0000

    Removed unnecessary types from ReportLine

--- blast_parser/KrakenParser/KrakenReportLine.swift
+++ blast_parser/KrakenParser/KrakenReportLine.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 struct ReportLine {
-    var lineNumber:Int = 0
-    var percentage:Float = 0.0
-    var reads:Int = 0
-    var assignedReads:Int = 0
+    var lineNumber = 0
+    var percentage = 0.0
+    var reads = 0
+    var assignedReads = 0
     var rank:Rank!
     var hierarchy:Hierarchy!
-    var taxID:Int = 0
+    var taxID = 0
     
     init() {}
     
