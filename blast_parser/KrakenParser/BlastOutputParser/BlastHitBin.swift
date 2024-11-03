f570c9da6d3b253162e224a08b73b04b529cd799
João Varela <joaocvarela@gmail.com>
2024-11-03 19:28:45 +0000

    Continued the implementation of the BLASTn output parser

--- blast_parser/KrakenParser/BlastOutputParser/BlastHitBin.swift
+++ blast_parser/KrakenParser/BlastOutputParser/BlastHitBin.swift
@@ -16,7 +16,31 @@ struct BlastHitBin {
     
     var hits:[BlastHit]
     
-    func sort(criterion:SortCriterion) {
-        
+    var bestHit:BlastHit? {
+        guard hits.isEmpty == false else { return nil }
+        return hits[0]
+    }
+    
+    mutating func sort(criterion:SortCriterion) {
+        switch criterion {
+        case .identity:
+            hits.sort(by: sortByIdentity)
+        case .eValue:
+            hits.sort(by: sortByEValue)
+        case .bitScore:
+            hits.sort(by: sortByBitScore)
+        }
+    }
+    
+    func sortByIdentity(_ lhs:BlastHit,_ rhs:BlastHit) -> Bool {
+        lhs.percentageIdentity > rhs.percentageIdentity
+    }
+    
+    func sortByEValue(_ lhs:BlastHit,_ rhs:BlastHit) -> Bool {
+        lhs.eValue < rhs.eValue
+    }
+    
+    func sortByBitScore(_ lhs:BlastHit,_ rhs:BlastHit) -> Bool {
+        lhs.bitscore > rhs.bitscore
     }
 }
--- blast_parser/KrakenParser/BlastOutputParser/BlastOutputParser.swift
+++ blast_parser/KrakenParser/BlastOutputParser/BlastOutputParser.swift
@@ -7,40 +7,44 @@
 
 import Foundation
 
-// BLASTn output format
-// A: qseqid = Query Seq-id (ID of your sequence)
-// B: sseqid = Subject Seq-id (ID of the database hit)
-// C: pident = Percentage of identical matches
-// D: length = Alignment length
-// E: evalue = Expected value (E-value)
-// F: bitscore = Bit score
-// G: sallseqid = All subject Seq-id(s), separated by a ';'
-// H: score = Raw score
-// I: nident = Number of identical matches
-// J: positive = Number of positive-scoring matches
-// K: ppos = Percentage of positive-scoring matches
-// L: qframe = Query frame
-// M: sframe = Subject frame
-// N: qlen = Query sequence length
-// O: slen = Subject sequence length
-// P: salltitles = All subject title(s), separated by a '<>'
-// Q: staxids = unique Subject Taxonomy ID(s), separated by a ';' (in numerical order)
-// R: sscinames = unique Subject Scientific Name(s), separated by a ';'
-// S: sskingdoms = unique Subject Super Kingdom(s), separated by a ';' (in alphabetical order)
-
 final class BlastOutputParser: FileParser {
-    let parsedClassification:String
+    let asvsParser:KrakenASVParser
     var hits = [BlastHit]()
+    var bins = [BlastHitBin]()
+    var blastASVs = [BlastASV]()
     
     init?(path:String, parsedClassification: String) {
-        self.parsedClassification = parsedClassification
+        guard let asvsParser = KrakenASVParser(path: parsedClassification)
+            else { return nil }
+        self.asvsParser = asvsParser
         super.init(path: path)
     }
     
     func parse() throws {
         try parseBlastOutput()
+        parseBins()
     }
     
+    /// Assumes a BLASTn output table with 19 columns as follows:
+    /// A: qseqid = Query Seq-id (ID of your sequence)
+    /// B: sseqid = Subject Seq-id (ID of the database hit)
+    /// C: pident = Percentage of identical matches
+    /// D: length = Alignment length
+    /// E: evalue = Expected value (E-value)
+    /// F: bitscore = Bit score
+    /// G: sallseqid = All subject Seq-id(s), separated by a ';'
+    /// H: score = Raw score
+    /// I: nident = Number of identical matches
+    /// J: positive = Number of positive-scoring matches
+    /// K: ppos = Percentage of positive-scoring matches
+    /// L: qframe = Query frame
+    /// M: sframe = Subject frame
+    /// N: qlen = Query sequence length
+    /// O: slen = Subject sequence length
+    /// P: salltitles = All subject title(s), separated by a '<>'
+    /// Q: staxids = unique Subject Taxonomy ID(s), separated by a ';' (in numerical order)
+    /// R: sscinames = unique Subject Scientific Name(s), separated by a ';'
+    /// S: sskingdoms = unique Subject Super Kingdom(s), separated by a ';' (in alphabetical order)
     private func parseBlastOutput() throws {
         for line in readStream {
             let items = line.components(separatedBy: "\t")
@@ -65,5 +69,30 @@ final class BlastOutputParser: FileParser {
                                kingdom: kingdom)
             hits.append(hit)
         }
+    }
+    
+    /// Parses the BLASTn output into bins with the same sequenceID
+    /// Assumes the hits are sorted by their sequenceIDs
+    private func parseBins() {
+        var previousID = String()
+        var binHits = [BlastHit]()
+        for hit in hits {
+            if hit.querySequenceID == previousID {
+                binHits.append(hit)
+            } else {
+                var bin = BlastHitBin(hits: binHits)
+                bin.sort(criterion: .bitScore)
+                bins.append(bin)
+                binHits = [BlastHit]()
+                previousID = hit.querySequenceID
+            }
+        }
+        hits = [BlastHit]()
+    }
+    
+    /// Merge Kraken ASVs with BLASTn best hit of each bin
+    private func merge() {
+        asvsParser.parse()
+        let asvs = asvsParser.binArray.getASVs()
     }
 }
