be4afd1228bb6c36498a0f1c9b47213b1cc1da24
João Varela <joaocvarela@gmail.com>
2024-11-16 20:57:31 +0000

    Fixed the correct setting up of the input files

--- bin/run2.sh
+++ bin/run2.sh
@@ -6,9 +6,9 @@ scripts_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
 root_dir=$(echo "$scripts_dir" | rev | cut -d'/' -f4- | rev)
 blast_parser="${scripts_dir}/blast_parser"
 sequences_dir="${root_dir}/18S-Nanopore/Sequences/ALL_02_120824"
-asvs_f="ALL_02_120824_parsed.tsv"
+asvs_f="kraken2-parsed-classification.tsv"
 blasthits_f="blast2.tsv"
-parsed_taxonomy_f="kraken2-parsed-classification.tsv"
+parsed_taxonomy_f="ALL_02_120824_parsed.tsv"
 
 cd "$sequences_dir"
 
