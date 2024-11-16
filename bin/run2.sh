b2d756c730c2428af3c214200352b679e36c4352
João Varela <joaocvarela@gmail.com>
2024-11-16 19:55:34 +0000

    Edit bash script to use the new version of the merge subcommand

--- bin/run2.sh
+++ bin/run2.sh
@@ -8,12 +8,14 @@ blast_parser="${scripts_dir}/blast_parser"
 sequences_dir="${root_dir}/18S-Nanopore/Sequences/ALL_02_120824"
 asvs_f="ALL_02_120824_parsed.tsv"
 blasthits_f="blast2.tsv"
+parsed_taxonomy_f="kraken2-parsed-classification.tsv"
 
 cd "$sequences_dir"
 
 "$blast_parser" merge \
   -a "$asvs_f" \
   -b "$blasthits_f" \
+  -p "$parsed_taxonomy_f" \
   -h 5 \
   -s "bitscore"
 
