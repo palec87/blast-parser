8c574c881346388fad10a66cb083b2de8b5cfd72
João Varela <joaocvarela@gmail.com>
2024-11-18 23:37:35 +0000

    Implemented the BLAST lineage using a Hierarchy object

--- blast_parser/BlastOutputParser/BlastASV.swift
+++ blast_parser/BlastOutputParser/BlastASV.swift
@@ -11,7 +11,7 @@ struct BlastASV: CustomStringConvertible {
     let asv:KrakenASV
     let hit:BlastHit
     let krakenTaxonomy:String?
-    var blastTaxonomy:String? = nil
+    var blastTaxonomy:Hierarchy? = nil
     
     init(asv: KrakenASV, taxonomy: String?, hit: BlastHit) {
         self.asv = asv
@@ -20,16 +20,47 @@ struct BlastASV: CustomStringConvertible {
     }
     
     mutating func setBlastTaxonomy(database:SQLDatabase) {
-        // FIXME: Implement it with NCBILineage and then parse the result
+        let taxID = self.asv.taxonomy.taxID
+        do {
+            if let lineage = NCBILineage(database: database, taxID: taxID) {
+                blastTaxonomy = Hierarchy()
+                let kingdom = try Rank.rank(abbreviation: "D",
+                                            name: lineage.superkingdom)
+                blastTaxonomy!.addRank(kingdom)
+                let phylum = try Rank.rank(abbreviation: "P",
+                                           name: lineage.phylum)
+                blastTaxonomy!.addRank(phylum)
+                let `class` = try Rank.rank(abbreviation: "C",
+                                            name: lineage.class)
+                blastTaxonomy!.addRank(`class`)
+                let order = try Rank.rank(abbreviation: "O",
+                                          name: lineage.order)
+                blastTaxonomy!.addRank(order)
+                let family = try Rank.rank(abbreviation: "F",
+                                           name: lineage.family)
+                blastTaxonomy!.addRank(family)
+                let genus = try Rank.rank(abbreviation: "G",
+                                          name: lineage.genus)
+                blastTaxonomy!.addRank(genus)
+                let species = try Rank.rank(abbreviation: "S",
+                                            name: lineage.species)
+                blastTaxonomy!.addRank(species)
+            }
+        }
+        
+        catch {
+            Console.writeToStdErr("ERROR: Invalid taxonomy for tax_id = \(taxID)")
+            blastTaxonomy = nil
+        }
     }
     
     var description:String {
-        if let blastTaxonomy = self.blastTaxonomy,
+        if let blastTaxonomy = self.blastTaxonomy?.getRanks(),
             let krakenTaxonomy = self.krakenTaxonomy {
             return "\(asv)\t\(krakenTaxonomy)\t\(hit)\t\(blastTaxonomy)"
         } else if let krakenTaxonomy = self.krakenTaxonomy {
             return "\(asv)\t\(krakenTaxonomy)\t\(hit)"
-        } else if let blastTaxonomy = self.blastTaxonomy {
+        } else if let blastTaxonomy = self.blastTaxonomy?.getRanks() {
             return "\(asv)\t\(hit)\t\(blastTaxonomy)"
         } else {
             return "\(asv)\t\(hit)"
--- blast_parser/TaxonomyDB/NCBILineage.swift
+++ blast_parser/TaxonomyDB/NCBILineage.swift
@@ -9,28 +9,28 @@
 struct NCBILineage {
     let taxId:Int
     let taxName:String
-    let species:String?
-    let genus:String?
-    let family:String?
-    let order:String?
-    let `class`:String?
-    let phylum:String?
-    let kingdom:String?
-    let superkingdom:String?
+    let species:String
+    let genus:String
+    let family:String
+    let order:String
+    let `class`:String
+    let phylum:String
+    let kingdom:String
+    let superkingdom:String
     
     init?(string:String) {
         let fields = string.components(separatedBy: kPSDQueryResultSeparator)
         guard fields.count == kPSDQueryFieldNumber else { return nil }
         self.taxId = Int(fields[0])!
-        self.taxName = fields[1]
-        self.species = fields[2]
-        self.genus = fields[3]
-        self.family = fields[4]
-        self.order = fields[5]
-        self.class = fields[6]
-        self.phylum = fields[7]
-        self.kingdom = fields[8]
-        self.superkingdom = fields[9]
+        self.taxName = fields[1].trimmingCharacters(in: .whitespaces)
+        self.species = fields[2].trimmingCharacters(in: .whitespaces)
+        self.genus = fields[3].trimmingCharacters(in: .whitespaces)
+        self.family = fields[4].trimmingCharacters(in: .whitespaces)
+        self.order = fields[5].trimmingCharacters(in: .whitespaces)
+        self.class = fields[6].trimmingCharacters(in: .whitespaces)
+        self.phylum = fields[7].trimmingCharacters(in: .whitespaces)
+        self.kingdom = fields[8].trimmingCharacters(in: .whitespaces)
+        self.superkingdom = fields[9].trimmingCharacters(in: .whitespaces)
     }
     
     init?(database:SQLDatabase, taxID:Int) {
